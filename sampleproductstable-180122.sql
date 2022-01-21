68b1a732f2852c92589e06bb457ef047c8f02610
ashwin-sonu <98007454+ashwin-sonu@users.noreply.github.com>
2022-01-21 15:31:04 +0530

    first commit

---
+++ sampleproductstable-180122.sql
@@ -0,0 +1,34 @@
+create database sampl
+Create Table tblProducts
+(
+ [Id] int identity primary key,
+ [Name] nvarchar(50),
+ [Description] nvarchar(250)
+)
+
+Create Table tblProductSales
+(
+ Id int primary key identity,
+ ProductId int foreign key references tblProducts(Id),
+ UnitPrice int,
+ QuantitySold int
+)
+
+Insert into tblProducts values ('TV', '52 inch black color LCD TV')
+Insert into tblProducts values ('Laptop', 'Very thin black color acer laptop')
+Insert into tblProducts values ('Desktop', 'HP high performance desktop')
+
+Insert into tblProductSales values(3, 450, 5)
+
+
+
+
+Insert into tblProductSales values(2, 250, 7)
+Insert into tblProductSales values(3, 450, 4)
+Insert into tblProductSales values(3, 450, 9)
+
+select Id,Name,Description from tblProducts where id not in 
+(Select ProductId from tblProductSales)
+
+select Name,(select SUM(QuantitySold)as quantity_sold from tblProductSales where ProductId=tblProducts.Id)
+from tblProducts
